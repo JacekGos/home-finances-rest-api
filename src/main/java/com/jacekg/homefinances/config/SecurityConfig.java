4de7826ea4cb241541b4d05e38c4622cb8f430d5
Jacekg1993 <jacekg93@gmail.com>
2021-11-17 01:23:55 +0100

    Added: Security config classes

---
+++ src/main/java/com/jacekg/homefinances/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.jacekg.homefinances.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+
+}
