4bef3e5fc3fe7897782cf6035121744f2b84a4fa
Jacekg1993 <jacekg93@gmail.com>
2021-11-17 02:33:29 +0100

    Added: config authentication class

--- src/main/java/com/jacekg/homefinances/config/SecurityConfig.java
+++ src/main/java/com/jacekg/homefinances/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.jacekg.homefinances.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
 	
 
 }
