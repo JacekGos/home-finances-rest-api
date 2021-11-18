f5dbcecb6ecc66b8cc18edd9a28c74678ad5db17
Jacekg1993 <jacekg93@gmail.com>
2021-11-18 04:53:47 +0100

    Added: UserRestController

---
+++ src/main/java/com/jacekg/homefinances/user/UserRestController.java
@@ -0,0 +1,12 @@
+package com.jacekg.homefinances.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserRestController {
+	
+	
+
+}
