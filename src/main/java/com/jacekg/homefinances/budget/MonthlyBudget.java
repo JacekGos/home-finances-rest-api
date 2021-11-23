a2aa033e7e2da28bde3139f79739150b16124336
Jacekg93 <jacekg93@gmail.com>
2021-11-23 16:28:05 +0100

    Added: MonthlyBudget class

--- src/main/java/com/jacekg/homefinances/budget/MonthlyBudget.java
+++ src/main/java/com/jacekg/homefinances/budget/MonthlyBudget.java
@@ -1,5 +1,46 @@
 package com.jacekg.homefinances.budget;
 
-public class MonthlyBudget {
+import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.jacekg.homefinances.expenses.ConstantExpense;
+import com.jacekg.homefinances.expenses.OneTimeExpense;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "monthly-budget")
+public class MonthlyBudget {
+	
+	private int id;
+	
+	private LocalDate date;
+	
+	private int previousMonthEarnings;
+	
+	private int finalBalance;
+	
+	List<ConstantExpense> constantExpenses;
+	
+	List<OneTimeExpense> oneTimeExpenses;
 }
+
+
+
+
+
+
+
+
+
+
