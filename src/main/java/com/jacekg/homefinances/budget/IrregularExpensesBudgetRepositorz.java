ee9a2245deea141f09a6d817366c3427feebe356
Jacekg93 <jacekg93@gmail.com>
2021-11-29 08:15:13 +0100

    Added: Controller, Service, Repository layes for MonthlyBudget , IrregulareExpensesBudget classes

---
+++ src/main/java/com/jacekg/homefinances/budget/IrregularExpensesBudgetRepositorz.java
@@ -0,0 +1,7 @@
+package com.jacekg.homefinances.budget;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IrregularExpensesBudgetRepositorz extends JpaRepository<IrregularExpensesBudget, Long> {
+
+}
