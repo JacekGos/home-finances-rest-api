33bd32ab4961a741aac81211dd4420630949de7c
Jacekg1993 <jacekg93@gmail.com>
2021-12-03 21:18:52 +0100

    Added: MonthlyBudgetRepository save method

--- src/main/java/com/jacekg/homefinances/budget/MonthlyBudgetRepository.java
+++ src/main/java/com/jacekg/homefinances/budget/MonthlyBudgetRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MonthlyBudgetRepository extends JpaRepository<MonthlyBudget, Long> {
 	
 	MonthlyBudget findByUserIdAndDate(Long userId, LocalDate date);
+	
+	MonthlyBudget save(MonthlyBudget monthlyBudget);
 }
