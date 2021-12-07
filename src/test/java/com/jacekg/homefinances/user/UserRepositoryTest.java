1bb011df77e0066389e541b43a219e71359e088b
Jacekg1993 <jacekg93@gmail.com>
2021-12-08 00:28:15 +0100

    Added: UserRepository findByUserId test

--- src/test/java/com/jacekg/homefinances/user/UserRepositoryTest.java
+++ src/test/java/com/jacekg/homefinances/user/UserRepositoryTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,6 +17,8 @@ class UserRepositoryTest {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	private User user;
 	
 	@Test
 	void findByUsername_ShouldReturn_User() {
@@ -40,13 +44,42 @@ class UserRepositoryTest {
 		
 		User returnedUser = userRepository.findByUsername(username);
 		
-		assertNotNull(returnedUser);
 		assertEquals("user", returnedUser.getUsername());
 	}
 
 	@Test
 	void testFindByUserId() {
-		fail("Not yet implemented");
+		
+		Long userID = 1L;
+		
+		User user = new User(
+				1L,
+				"user",
+				"password",
+				"name",
+				"lastName",
+				"email@",
+				true,
+				true,
+				true,
+				true, 
+				null, 
+				null, 
+				Arrays.asList(new Role(1L, "ROLE_USER"), new Role(2L, "ROLE_ADMIN")));
+		
+		userRepository.save(user);
+		
+		User returnedUser = userRepository.findByUserId(userID);
+		
+		assertEquals(1L, returnedUser.getId());
 	}
-
 }
+
+
+
+
+
+
+
+
+
