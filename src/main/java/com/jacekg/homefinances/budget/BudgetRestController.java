a52293b0a026e857176cec5571cc5325c99ce2d0
Jacekg1993 <jacekg93@gmail.com>
2021-12-17 22:12:19 +0100

    Modified: deleteByDate method

--- src/main/java/com/jacekg/homefinances/budget/BudgetRestController.java
+++ src/main/java/com/jacekg/homefinances/budget/BudgetRestController.java
@@ -6,14 +6,11 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import static org.springframework.http.HttpStatus.OK;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
