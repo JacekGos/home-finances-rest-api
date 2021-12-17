bef1ec8cc838127edc5e449864b4d95680f8d1a4
Jacekg93 <jacekg93@gmail.com>
2021-12-17 11:44:19 +0100

    Modifed: BudgetUtilities removeDuplicatedExpenses method - problem with Long type comparing

--- src/main/java/com/jacekg/homefinances/budget/BudgetUtilities.java
+++ src/main/java/com/jacekg/homefinances/budget/BudgetUtilities.java
@@ -9,31 +9,7 @@ import com.jacekg.homefinances.expenses.model.Expense;
 
 public class BudgetUtilities {
 	
-//	public static List<ConstantExpense> removeDuplicatedConstantExpenses(List<ConstantExpense> currentConstantExpenses,
-//			List<ConstantExpense> updatedConstantExpenses) {
-//
-//		ConstantExpense currentConstantExpense;
-//		ConstantExpense updatedConstantExpense;
-//
-//		for (int i = 0; i < updatedConstantExpenses.size(); i++) {
-//
-//			updatedConstantExpense = updatedConstantExpenses.get(i);
-//
-//			for (int j = 0; j < currentConstantExpenses.size(); j++) {
-//
-//				currentConstantExpense = currentConstantExpenses.get(j);
-//
-//				if (updatedConstantExpense.getId() != (currentConstantExpense.getId())
-//						&& updatedConstantExpense.getName().equals(currentConstantExpense.getName())) {
-//					updatedConstantExpenses.remove(i);
-//				}
-//			}
-//		}
-//
-//		return updatedConstantExpenses;
-//	}
-	
-	public static <T> List<T> removeDuplicatedExpenses(List<T> currentExpenses,
+	public static <T extends Expense> List<T> removeDuplicatedExpenses(List<T> currentExpenses,
 			List<T> updatedExpenses) {
 		
 		System.out.println("currentExpense: " + currentExpenses);
@@ -45,15 +21,21 @@ public class BudgetUtilities {
 		for (int i = 0; i < updatedExpenses.size(); i++) {
 
 			updatedExpense = updatedExpenses.get(i);
-			System.out.println("updated:" + updatedExpense.toString());
+			System.out.println("updated:" + updatedExpense + " id: " + updatedExpense.getId());
 			
 			for (int j = 0; j < currentExpenses.size(); j++) {
 
 				currentExpense = currentExpenses.get(j);
-				System.out.println("current: " + currentExpense);
+				System.out.println("current: " + currentExpense + " id: " + currentExpense.getId());
 				
-				if (((Expense) updatedExpense).getId() != (((Expense) currentExpense).getId())
-						&& ((Expense) updatedExpense).getName().equals(((Expense) currentExpense).getName())) {
+//				if (updatedExpense.getId() == null) {
+//					System.out.println("jest null");
+//				}
+				
+				if ((updatedExpense.getId() == null 
+						|| !(updatedExpense.getId().equals((currentExpense.getId()))))
+						&& (updatedExpense.getName().equals(currentExpense.getName()))) {
+					System.out.println("removed:" + updatedExpenses.get(i));
 					updatedExpenses.remove(i);
 				}
 			}
@@ -61,28 +43,6 @@ public class BudgetUtilities {
 
 		return updatedExpenses;
 	}
-	
-//	public static List<Long> findConstantExpensesIdToRemove
-//		(Collection<ConstantExpense> currentConstantExpenses,
-//		Collection<ConstantExpense> updatedConstantExpenses) {
-//
-//		List<Long> constantExpensesIdToRemove = new ArrayList<>();
-//
-//		for (ConstantExpense constantExpense : currentConstantExpenses) {
-//			
-//			ConstantExpense searchedConstantExpense = updatedConstantExpenses
-//					.stream()
-//					.filter(
-//					updatedConstantExpense -> constantExpense.getName().equals(updatedConstantExpense.getName()))
-//					.findFirst().orElse(null);
-//
-//			if (searchedConstantExpense == null) {
-//				constantExpensesIdToRemove.add(constantExpense.getId());
-//			}
-//		}
-//
-//		return constantExpensesIdToRemove;
-//	}
 	
 	public static <T> List<Long> findExpensesIdsToRemove(
 			Collection<T> currentExpenses, Collection<T> updatedExpenses) {
