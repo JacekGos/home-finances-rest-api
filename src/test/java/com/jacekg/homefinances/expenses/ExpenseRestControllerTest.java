10ec1b8b786ace4b5355d0f0a7b76a3103b5f982
Jacekg93 <jacekg93@gmail.com>
2022-01-04 21:31:21 +0100

    Added: ExpenseRestControllerTest - addUserPreferenceConstantExpense_ShouldReturn_SatusCreated test

---
+++ src/test/java/com/jacekg/homefinances/expenses/ExpenseRestControllerTest.java
@@ -0,0 +1,56 @@
+package com.jacekg.homefinances.expenses;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.doNothing;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.jacekg.homefinances.expenses.model.ConstantExpenseDTO;
+import com.jacekg.homefinances.user.User;
+import com.jacekg.homefinances.user.UserService;
+
+@WebMvcTest(ExpenseRestController.class)
+@AutoConfigureMockMvc
+class ExpenseRestControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	ExpenseService expenseService;
+	
+	@MockBean
+	UserService userService;
+	
+	@Test
+	void addUserPreferenceConstantExpense_ShouldReturn_SatusCreated() throws Exception {
+		
+		User user = new User();
+		ConstantExpenseDTO constantExpenseDTO = new ConstantExpenseDTO();
+		
+//		when(expenseService.addUserPreferenceConstantExpense
+//				(any(ConstantExpenseDTO.class), any(User.class)))
+		
+		doNothing().when(expenseService).addUserPreferenceConstantExpense(constantExpenseDTO, user);
+		
+		String url = "/expenses";
+		
+		mockMvc.perform(post(url))
+			.andExpect(status().isCreated());
+	}
+
+	@Test
+	void testRemoveUserPreferenceConstantExpense() {
+		fail("Not yet implemented");
+	}
+
+}
